7469b094106b882a7bac17f93a6c2182c031b5d1
wmau <wmau@bu.edu>
2016-12-20 14:20:49 -0500

    T4 changes

--- batch_align_pos.m
+++ batch_align_pos.m
@@ -79,78 +79,63 @@ global MasterDirectory;
 MasterDirectory = 'C:\MasterData';
 
 %% Parameters/default values
-manual_rot_overwrite = 1;
-ratio_use = 0.95; % ratio of the data to use for alignment - if 0.95, then
-% data is scaled so that the middle 95% of it in each session aligns with
-% the middle 95% in other sessions
-auto_rotate_to_std = 0;
-manual_limits = zeros(1,length(reg_struct) + 1);
-xmin = 10; % where you want to set the minimum x value
-ymin = 20; % where you want to set the minimum y-value
+p = inputParser;
+p.addRequired('base_struct',@(x) isstruct(x));
+p.addRequired('reg_struct',@(x) isstruct(x)); 
+p.addParameter('manual_rot_overwrite',true,@(x) islogical(x));
+p.addParameter('ratio_use',0.95,@(x) isscalar(x)); 
+p.addParameter('auto_rotate_to_std',false,@(x) islogical(x)); 
+p.addParameter('manual_limits',zeros(1,length(reg_struct)+1),@(x) islogical(x));
+p.addParameter('name_append','',@(x) ischar(x)); 
+p.addParameter('circ2square_use',false,@(x) islogical(x)); 
+p.addParameter('TenaspisVer',4,@(x) isscalar(x) & x>2); 
 
-%% 0: Get varargins
-
-name_append = ''; % default
-circ2square_use = 0; % default
-halfwindow = 0; % default for Tenaspis2, use 10 for Tenaspis1
-for j = 1:length(varargin)
-   if strcmpi(varargin{j},'manual_rot_overwrite')
-       manual_rot_overwrite = varargin{j+1};
-   end
-   if strcmpi(varargin{j},'ratio_use')
-       ratio_use = varargin{j+1};
-   end
-   if strcmpi(varargin{j},'auto_rotate_to_std')
-       auto_rotate_to_std = varargin{j+1};
-   end
-   if strcmpi(varargin{j},'manual_limits')
-      manual_limits = varargin{j+1}; 
-      % Make the same length as all the reg_struct sessions + 1
-      if length(manual_limits) ~= (length(reg_struct) + 1)
-          manual_limits(1:(length(reg_struct) + 1)) = manual_limits;
-      end
-   end
-   if strcmpi(varargin{j},'name_append')
-      name_append = varargin{j+1}; 
-   end
-   if strcmpi(varargin{j},'circ2square_use')
-      circ2square_use = varargin{j+1};
-      name_append = '_trans';
-   end 
-   if strcmpi(varargin{j},'halfwindow')
-       halfwindow = varargin{j+1};
-   end
-end
+p.parse(base_struct,reg_struct,varargin{:});
+manual_rot_overwrite = p.Results.manual_rot_overwrite;
+ratio_use = p.Results.ratio_use;
+auto_rotate_to_std = p.Results.auto_rotate_to_std;
+manual_limits = p.Results.manual_limits;
+name_append = p.Results.name_append;
+circ2square_use = p.Results.circ2square_use;
+TenaspisVer = p.Results.TenaspisVer;
+xmin = 10; ymin = 20;
 
 %% 1: Load all sessions, and align to imaging data
 
 % Dump everything into one structure for future ease
-sesh(1) = base_struct;
-sesh(2:length(reg_struct) + 1) = reg_struct;
+sesh = [base_struct, reg_struct];
 
-currdir = cd;
 for j = 1: length(sesh)
-    ChangeDirectory(sesh(j).Animal, sesh(j).Date ,sesh(j).Session);
+    cd(sesh(j).Location);
     Pix2Cm = sesh(j).Pix2CM; 
     disp(['Using ', num2str(Pix2Cm), ' as Pix2CM for ', sesh(j).Date, ' session ', num2str(sesh(j).Session)]);
     
-    try
+    if TenaspisVer==4
+        disp('Loading results from Tenaspis v4.');
+        HalfWindow = 0;
+        load(fullfile(pwd,'FinalOutput.mat'),'PSAbool','NeuronTraces');  
+        LPtrace = NeuronTraces.LPtrace;
+        DFDTtrace = NeuronTraces.DFDTtrace; 
+        RawTrace = NeuronTraces.RawTrace;
+        clear NeuronTraces;
+    elseif TenaspisVer==3
+        disp('Loading results from Tenaspis v3.');
+        HalfWindow = 0;
         load(fullfile(pwd,'FinalOutput.mat'),'FT');
         load(fullfile(pwd,'FinalTraces.mat'),'trace','difftrace','rawtrace');
-        disp('Using FinalOutput.');
-        HalfWindow = 0; 
-    catch
-        load('ProcOut.mat', 'FT');
-        disp('Using ProcOut.');
-        HalfWindow = 10;
+        PSAbool = FT;
+        LPtrace = trace;
+        DFDTtrace = difftrace; 
+        RawTrace = rawtrace;
+        clear FT trace difftrace rawtrace;
     end
     
     % Align tracking and imaging
-    [x,y,speed,FT,FToffset,FToffsetRear,aviFrame,time_interp,nframesinserted] = ...
-        AlignImagingToTracking(Pix2Cm,FT,HalfWindow);
-    [~,~,~,trace] = AlignImagingToTracking(Pix2Cm,trace,HalfWindow);
-    [~,~,~,difftrace] = AlignImagingToTracking(Pix2Cm,difftrace,HalfWindow);
-    [~,~,~,rawtrace] = AlignImagingToTracking(Pix2Cm,rawtrace,HalfWindow);
+    [x,y,speed,PSAbool,FToffset,FToffsetRear,aviFrame,time_interp,nframesinserted] = ...
+        AlignImagingToTracking(Pix2Cm,PSAbool,HalfWindow);
+    [~,~,~,LPtrace] = AlignImagingToTracking(Pix2Cm,LPtrace,HalfWindow);
+    [~,~,~,DFDTtrace] = AlignImagingToTracking(Pix2Cm,DFDTtrace,HalfWindow);
+    [~,~,~,RawTrace] = AlignImagingToTracking(Pix2Cm,RawTrace,HalfWindow);
     
 %     % Transform circle data if indicated AND if in the square
 %     if circ2square_use == 1 && ~isempty(regexpi(sesh(j).Env,'octagon')) 
@@ -165,10 +150,10 @@ for j = 1: length(sesh)
     
     sesh(j).x = x;
     sesh(j).y = y;
-    sesh(j).FT = FT;
-    sesh(j).trace = trace;
-    sesh(j).difftrace = difftrace;
-    sesh(j).rawtrace = rawtrace;
+    sesh(j).PSAbool = PSAbool;
+    sesh(j).LPtrace = LPtrace;
+    sesh(j).DFDTtrace = DFDTtrace;
+    sesh(j).RawTrace = RawTrace;
     sesh(j).speed = speed;
     sesh(j).FToffset = FToffset;
     sesh(j).FToffsetRear = FToffsetRear;
@@ -188,10 +173,8 @@ for j = 1: length(sesh)
     sesh(j).aviFrame = aviFrame;
     sesh(j).time_interp = time_interp;
     sesh(j).nframesinserted = nframesinserted;
-    
 end
 
-% keyboard
 
 %% 2: Align position data for each session to the base session by using the 95% occupancy limits, save as Pos_align.mat
 % Include base session in Pos_align for future reference
@@ -202,17 +185,17 @@ end
 
 for j = 1:length(sesh)
     
-    if manual_limits(j) == 0
+    if ~manual_limits(j)
         x_for_limits = sesh(j).rot_x;
         y_for_limits = sesh(j).rot_y;
-        sesh(j).ind_keep = logical(ones(1,length(sesh(j).rot_x)));
-    elseif manual_limits(j) == 1
+        sesh(j).ind_keep = true(1,length(sesh(j).rot_x));
+    elseif manual_limits(j)
         [x_for_limits, y_for_limits, sesh(j).ind_keep] = draw_manual_limits(...
             sesh(j).rot_x, sesh(j).rot_y);
     end
     
     % Transform circle to square if indicated
-    if circ2square_use == 1 && ~isempty(regexpi(sesh(j).Env,'octagon')) 
+    if circ2square_use && ~isempty(regexpi(sesh(j).Env,'octagon')) 
         %Arena Size Parameters
         circle_radius = 14.33;
         square_side = 25.4;
@@ -223,7 +206,6 @@ for j = 1:length(sesh)
         sesh(j).rot_x(sesh(j).ind_keep) = x_for_limits; 
         sesh(j).rot_y(sesh(j).ind_keep) = y_for_limits;
     end
-    
     
     % Get ecdfs of all x and y points
     [sesh(j).e_fx, sesh(j).e_x] = ecdf(x_for_limits);
@@ -247,8 +229,7 @@ for j = 1:length(sesh)
     
     % Linearly adjust all the coordinates to match - use all position data!
     sesh(j).x_adj = (sesh(j).rot_x - xbound{j}(1))/span_x_ratio + xmin;
-    sesh(j).y_adj = (sesh(j).rot_y - ybound{j}(1))/span_y_ratio + ymin;
-    
+    sesh(j).y_adj = (sesh(j).rot_y - ybound{j}(1))/span_y_ratio + ymin; 
 end
 %% 4: Concatenate ALL position data into one X and one Y vector, and get Xedges and Yedges based on this
 
@@ -269,31 +250,35 @@ ymin = min(y_all);
 %% 6: Save Xedges, Yedges in base session for future reference along with all sessions aligned to it.
 % Also save adjusted position data for future use...
 
-sessions_included(1) = base_struct;
-sessions_included(2:length(reg_struct) + 1) = reg_struct;
+sessions_included = [base_struct reg_struct];
 
 for j = 1:length(sesh)
     x_adj_cm = sesh(j).x_adj;
     y_adj_cm = sesh(j).y_adj;
     speed = sesh(j).speed;
-    FT = sesh(j).FT;
-    trace = sesh(j).trace;
-    difftrace = sesh(j).difftrace;
-    rawtrace = sesh(j).rawtrace;
+    PSAbool = sesh(j).PSAbool;
+    LPtrace = sesh(j).LPtrace;
+    DFDTtrace = sesh(j).DFDTtrace;
+    RawTrace = sesh(j).RawTrace;
     FToffset = sesh(j).FToffset;
     FToffsetRear = sesh(j).FToffsetRear;
     aviFrame = sesh(j).aviFrame;
     time_interp = sesh(j).time_interp;
     nframesinserted = sesh(j).nframesinserted;
-    if auto_rotate_to_std == 0
-    save(fullfile(sesh(j).Location,['Pos_align' name_append '.mat']),'x_adj_cm','y_adj_cm',...
-        'xmin','xmax','ymin','ymax', 'speed', 'FT', 'trace', 'difftrace', 'rawtrace','FToffset', 'nframesinserted','time_interp',...
-        'FToffsetRear', 'aviFrame', 'base_struct','sessions_included','auto_rotate_to_std');
-    elseif auto_rotate_to_std == 1
+    if ~auto_rotate_to_std
+    save(fullfile(sesh(j).Location,['Pos_align' name_append '.mat']),...
+        'x_adj_cm','y_adj_cm','xmin','xmax','ymin','ymax','speed',...
+        'PSAbool','LPtrace','DFDTtrace','RawTrace','FToffset',...
+        'nframesinserted','time_interp','FToffsetRear','aviFrame',...
+        'base_struct','sessions_included','auto_rotate_to_std');
+    elseif auto_rotate_to_std
         % finish here - save as a different filename?
-        save(fullfile(sesh(j).Location,['Pos_align_std_corr' name_append '.mat']),'x_adj_cm','y_adj_cm',...
-        'xmin','xmax','ymin','ymax', 'speed', 'FT', 'trace', 'difftrace','rawtrace','FToffset', 'nframesinserted','time_interp',...
-        'FToffsetRear','aviFrame', 'base_struct', 'sessions_included', 'auto_rotate_to_std');
+        save(fullfile(sesh(j).Location,...
+            ['Pos_align_std_corr' name_append '.mat']),'x_adj_cm',...
+            'y_adj_cm','xmin','xmax','ymin','ymax','speed','PSAbool',...
+            'LPtrace','DFDTtrace','RawTrace','FToffset','nframesinserted',...
+            'time_interp','FToffsetRear','aviFrame','base_struct',...
+            'sessions_included', 'auto_rotate_to_std');
     end
 end
 
